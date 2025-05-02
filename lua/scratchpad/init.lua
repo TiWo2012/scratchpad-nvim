7bff7e3aa79e31b1387ccdafda07c5a3d625ad5f
TiWo2012 <timo.wolf@gmx.ch>
2025-05-02 17:35:50 +0200

    feat: better setup

--- lua/scratchpad/init.lua
+++ lua/scratchpad/init.lua
@@ -1,15 +1,22 @@
 local api = vim.api
 local buf = nil
-local options = { language=nil }
+local options = { language = nil }
 
 local M = {}
 
-local function open()
+local function createBuf()
   buf = api.nvim_create_buf(true, true)
 
   api.nvim_buf_set_name(buf, "scratchpad")
-
   api.nvim_buf_set_option(buf, "fliletype", options.language)
+end
+
+local function open()
+  if buf == nil then
+    createBuf()
+  else
+    api.nvim_open_win(buf, true, {})
+  end
 end
 
 local function createUserCommands()
@@ -23,7 +30,6 @@ local function createUserCommands()
 end
 
 function M.setup(opts)
-
   options = opts
   createUserCommands()
 end
